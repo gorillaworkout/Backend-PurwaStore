910ce5b1e32e7a6ccf0a0670e3ae8e6ea8e76445
Hannah <hannah4669@users.noreply.github.com>
2020-10-13 21:59:29 +0700

    updet

--- Controllers/AdminControllers.js
+++ Controllers/AdminControllers.js
@@ -1,12 +1,14 @@
 const {db}=require('./../Connection')
 
 module.exports = {
-    getAdminData : (req, res)=>{
-        let sql = `select t.id, tanggalPembayaran, status, buktiPembayaran, SUM(price) as totalPrice
+    getWaitingAdmin : (req, res)=>{
+        let sql = `select t.id, t.tanggalPembayaran,status,buktiPembayaran, t.metode, u.namaLengkap, SUM(price) as totalPrice
                     from transactions t
                     join transactiondetails td
                     on t.id = td.transactionId
-                    where t.metode = 'upload'
+                    join users u
+                    on t.userId = u.id
+                    where t.status = 'WaitingAdmin'
                     group by t.id;`
         // console.log('asa')
         db.query(sql, (err, dataUpload)=>{
@@ -16,7 +18,36 @@ module.exports = {
             db.query(sql, (err, alldataProd)=>{
                 if(err) return res.status(500).send(err)
 
-                res.status(200).send({dataUpload, alldataProd})
+                sql = `select t.id, t.tanggalPembayaran,status,buktiPembayaran, t.metode, u.namaLengkap, SUM(price) as totalPrice
+                        from transactions t
+                        join transactiondetails td
+                        on t.id = td.transactionId
+                        join users u
+                        on t.userId = u.id
+                        where t.status = 'completed' or t.status = 'failed'
+                        group by t.id;`
+                db.query(sql, (err, dataCompleted)=>{
+                    if(err)return res.status(500).send(err)
+                    
+                    sql = `select t.id, u.namaLengkap, p.namaHp, p.gambar , td.qty, td.price, (td.qty * td.price) as subtotal
+                            from transactions t
+                            join transactiondetails td
+                            on t.id = td.transactionId
+                            join users u
+                            on t.userId = u.id
+                            join products p
+                            on td.productId = p.id
+                            order by t.id;`
+
+                    db.query(sql, (err, detailTrans)=>{
+                        if(err)return res.status(500).send(err)
+
+                        return res.status(200).send({
+                            alldataProd, dataUpload, dataCompleted, detailTrans
+                        })
+                    })
+                })
+                
             })
         })
     },
@@ -33,12 +64,14 @@ module.exports = {
                 db.query(sql, data, (err)=>{
                     if(err)return res.status(500).send(err)
 
-                    sql = `select t.id, tanggalPembayaran, status, buktiPembayaran, SUM(price) as totalPrice
+                    sql = `select t.id, t.tanggalPembayaran,status,buktiPembayaran, t.metode, u.namaLengkap, SUM(price) as totalPrice
                             from transactions t
                             join transactiondetails td
                             on t.id = td.transactionId
-                            where t.metode = 'upload'
-                            group by t.id`
+                            join users u
+                            on t.userId = u.id
+                            where t.status = 'completed' or t.status = 'failed'
+                            group by t.id;`
                     db.query(sql, (err, allProducts)=>{
                         if(err)return res.status(500).send(err)
                         return res.status(200).send(allProducts)
--- Routes/AdminRoutes.js
+++ Routes/AdminRoutes.js
@@ -2,7 +2,7 @@ const Router = require('express').Router()
 const { AdminRoutes } = require('.')
 const {AdminControllers} = require('./../Controllers')
 
-Router.get('/getAdminData', AdminControllers.getAdminData)
+Router.get('/getAdminData', AdminControllers.getWaitingAdmin)
 Router.post('/accBuktiTransfer/:id', AdminControllers.accBuktiPembayaran)
 
 module.exports=Router
